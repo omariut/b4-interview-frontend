a4868f1c6bbbe70d94075ec194fabce243b81535
Omar Faruk <omarcpgcbl@gmail.com>
2026-07-08 12:21:48 +0600

    feat: integrate main routes and layout wrapper in App

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Sidebar from './components/Layout/Sidebar';
+
+// New Pages
+import Dashboard from './pages/Dashboard/Dashboard';
+import CVUpload from './pages/CVUpload/CVUpload';
+import CVList from './pages/CVList/CVList';
+import CVDetails from './pages/CVDetails/CVDetails';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+
+        {/* Protected/Main App Routes wrapped in Sidebar Layout */}
+        <Route element={<Sidebar />}>
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/upload-cv" element={<CVUpload />} />
+          <Route path="/cvs" element={<CVList />} />
+          <Route path="/cvs/:id" element={<CVDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
